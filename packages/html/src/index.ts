808b926d338820b5f5e542ba5645c1ec2e72a0b8
kazurayam <kazuaki.urayama@gmail.com>
2026-02-22 21:50:18 +0900

    #9 packages/html

--- packages/html/src/index.ts
+++ packages/html/src/index.ts
@@ -2,6 +2,20 @@ import { Hono } from 'hono'
 import { html, raw } from 'hono/html'
 
 const app = new Hono()
+    .get('/', (c) => {
+        return c.html(
+            html`<!doctype html>
+                <html>
+                    <head>
+                        <title>Welcome nobody</title>
+                    </head>
+                    <body>
+                        <div>You are supposed to make queries like "GET /:username"</div>
+                    </body>
+                </html>
+            `
+        )
+    })
     .get('/:username', (c) => {
         const { username } = c.req.param()
         return c.html(
