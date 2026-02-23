fce40af038aafb3cc6f3cda81c7496209f07ccb3
kazurayam <kazuaki.urayama@gmail.com>
2026-02-23 14:08:22 +0900

    #4 Metadata hoisting

--- packages/jsx/src/index.tsx
+++ packages/jsx/src/index.tsx
@@ -6,40 +6,43 @@ import type { FC } from 'hono/jsx'
 // Function ComponentとはJSXを返す関数をReactコンポーネントとしてみるという意味。
 
 const app = new Hono()
-    .get('/', (c) => {
-        // cはContextオブジェクト
-        // https://hono.dev/docs/api/context
-        // The Context object is instantiated for each request and kept
-        // until the response is returned. You can put values in it,
-        // set headers and a status code you want to return, and access
-        // HonoRequest and Response objects.
-        const messages = ['Good Morning', 'Good Evening', 'Good Night']
-        return c.html(<Top messages={messages} />)
+
+app.use('*', async (c, next) => {
+    // cはContextオブジェクト
+    // https://hono.dev/docs/api/context
+    // The Context object is instantiated for each request and kept
+    // until the response is returned. You can put values in it,
+    // set headers and a status code you want to return, and access
+    // HonoRequest and Response objects.
+    c.setRenderer((content) => {
+        return c.html(
+            <html>
+                <head></head>
+                <body>{content}</body>
+            </html>
+        )
     })
+    await next()
+})
+
+app.get('/', (c) => {
+    const messages = ['Good Morning', 'Good Evening', 'Good Night']
+    return c.render(<Top messages={messages} />)
+})
 
 const Top: FC<{ messages: string[] }> = (props: {
     messages: string[]
 }) => {
     return (
-        <Layout>
+        <>
             <h1>Hello Hono!</h1>
             <ul>
                 {props.messages.map((message) => {
                     return <li>{message}!!</li>
                 })}
             </ul>
-        </Layout>
+        </>
     )
 }
-
-const Layout: FC = (props) => {
-    return (
-        <html>
-            <body>{props.children}</body>
-        </html>
-    )
-}
-
-
 
 export default app
