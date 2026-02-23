a06263370833bb516b9fe8e71fb2d7e33d496e78
kazurayam <kazuaki.urayama@gmail.com>
2026-02-23 13:44:04 +0900

    #4

--- packages/jsx/README.md
+++ packages/jsx/README.md
@@ -2,3 +2,7 @@
 
 [Honoドキュメント/JSX](https://hono.dev/docs/guides/jsx)を写経する
 
+
+## 参考情報
+
+[HonoでJSXを使ってHTMLを作ろう](https://qiita.com/morioka1206/items/f11ee596fc8175e23a62)
--- packages/jsx/src/index.tsx
+++ packages/jsx/src/index.tsx
@@ -1,17 +1,21 @@
 // packages/jsx/src/index.tsx
 
 import { Hono } from 'hono'
-import type { FC } from 'hono/jsx'  // 'FC' stands for "Fetch Client"
+import type { FC } from 'hono/jsx'
+// FCとはReact用語のFunction Componentの略。
+// Function ComponentとはJSXを返す関数をReactコンポーネントとしてみるという意味。
 
 const app = new Hono()
-
-const Layout: FC = (props) => {
-    return (
-        <html>
-            <body>{props.children}</body>
-        </html>
-    )
-}
+    .get('/', (c) => {
+        // cはContextオブジェクト
+        // https://hono.dev/docs/api/context
+        // The Context object is instantiated for each request and kept
+        // until the response is returned. You can put values in it,
+        // set headers and a status code you want to return, and access
+        // HonoRequest and Response objects.
+        const messages = ['Good Morning', 'Good Evening', 'Good Night']
+        return c.html(<Top messages={messages} />)
+    })
 
 const Top: FC<{ messages: string[] }> = (props: {
     messages: string[]
@@ -21,16 +25,21 @@ const Top: FC<{ messages: string[] }> = (props: {
             <h1>Hello Hono!</h1>
             <ul>
                 {props.messages.map((message) => {
-                    return <li>{ message }!!</li>
+                    return <li>{message}!!</li>
                 })}
             </ul>
         </Layout>
     )
 }
 
-app.get('/', (c) => {
-    const messages = ['Good Morning', 'Good Evening', 'Good Night']
-    return c.html(<Top messages={ messages } />)
-})
+const Layout: FC = (props) => {
+    return (
+        <html>
+            <body>{props.children}</body>
+        </html>
+    )
+}
+
+
 
 export default app
