ccf83c619ea88a067717a235624d5d9e556a196b
kazurayam <kazuaki.urayama@gmail.com>
2026-02-26 11:24:29 +0900

    #10 Basic

--- packages/routing/src/index.test.ts
+++ packages/routing/src/index.test.ts
@@ -2,9 +2,99 @@
 import { describe, expect, test } from 'bun:test';
 import app from './index';
 
-describe('HTTP Methods', () => {
-    test("GET /", async () => {
-        const res = await app.request("/", { method: 'GET' })
-        expect(res.status).toBe(200)
+describe('Basic', () => {
+    describe('HTTP Methods', () => {
+        test("GET /", async () => {
+            const res = await app.request("/", { method: 'GET' })
+            expect(res.status).toBe(200)
+        })
+        test("POST /", async () => {
+            const res = await app.request("/", { method: 'POST' })
+            expect(res.status).toBe(200)
+        })
+        test("PUT /", async () => {
+            const res = await app.request("/", { method: 'PUT' })
+            expect(res.status).toBe(200)
+        })
+        test("DELETE /", async () => {
+            const res = await app.request("/", { method: 'DELETE' })
+            expect(res.status).toBe(200)
+        })
     })
+
+    describe('Wildcard', () => {
+        test('GET /wild/foo/card', async () => {
+            const res = await app.request("/wild/foo/card", { method: "GET" })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('GET /wild/*/card')
+        })
+    })
+
+    describe('Any HTTP methods', () => {
+        test('GET /hello', async () => {
+            const res = await app.request('/hello', { method: 'GET' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Any Method /hello')
+        })
+        test('POST /hello', async () => {
+            const res = await app.request('/hello', { method: 'POST' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Any Method /hello')
+        })
+        test('PUT /hello', async () => {
+            const res = await app.request('/hello', { method: 'PUT' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Any Method /hello')
+        })
+        test('DELETE /hello', async () => {
+            const res = await app.request('/hello', { method: 'DELETE' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Any Method /hello')
+        })
+    })
+
+    describe('Custom HTTP method', () => {
+        test('PURGE /cache', async () => {
+            const res = await app.request('/cache', { method: 'PURGE' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('PURGE Method /cache')
+        })
+    })
+
+    /*
+    describe('Multiple Method', () => {
+        test('PUT /post', async () => {
+            const res = await app.request('/post', { method: 'PUT' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('PUT or DELETE /post')
+        })
+        test('DELETE /post', async () => {
+            const res = await app.request('/post', { method: 'DELETE' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('PUT or DELETE /post')
+        })
+    })
+    */
+
+    /*
+    describe('Multiple Paths', () => {
+        test('GET /hi', async () => {
+            const res = await app.request('/hi', { method: 'GET' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Hello')
+        })
+        test('GET /ja/hi', async () => {
+            const res = await app.request('/ja/hi', { method: 'GET' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Hello')
+        })
+        test('GET /en/hi', async () => {
+            const res = await app.request('/en/hi', { method: 'GET' })
+            expect(res.status).toBe(200)
+            expect(await res.text()).toBe('Hello')
+        })
+    })
+        */
 })
+
+
--- packages/routing/src/index.tsx
+++ packages/routing/src/index.tsx
@@ -2,10 +2,34 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+// HTTP Methods
 app.get('/', (c) => c.text('GET /'))
 app.post('/', (c) => c.text('POST /'))
 app.put('/', (c) => c.text('PUT /'))
 app.delete('/', (c) => c.text('DELETE /'))
 
+// Wildcard
+app.get('/wild/*/card', (c) => {
+    return c.text('GET /wild/*/card')
+})
+
+// Any HTTP methods
+app.all('/hello', (c) => c.text('Any Method /hello'))
+
+// Custom HTTP method
+app.on('PURGE', '/cache', (c) => c.text('PURGE Method /cache'))
 export default app
 
+// Multiple Methods
+/*
+app.on(['PUT', 'DELETE'], '/post', (c) => {
+    c.text('PUT or DELETE /post')
+})
+*/
+
+/* No overload matcheres here
+// Multiple Paths
+app.on('GET', ['/hi', '/ja/hi', '/en/hi'], (c) => {
+    c.text('Hello')
+})
+    */
