7b42e8c09553b480fe4f25fe5ee3eb965e335565
kazurayam <kazuaki.urayama@gmail.com>
2026-02-26 09:00:24 +0900

    #4

--- packages/jsx-renderer/src/index.tsx
+++ packages/jsx-renderer/src/index.tsx
@@ -6,29 +6,35 @@ when rendering JSX with the c.render() function, without
 the need for using c.setRenderer().
 Additionally, it enables access to instances of Context
 within components through the use of useRequestContext().
+See https://hono.dev/docs/middleware/builtin/jsx-renderer
  */
 import { Hono } from 'hono'
-import { jsxRenderer, useRequestContext } from 'hono/jsx-renderer'
+import { jsxRenderer } from 'hono/jsx-renderer'
 import { Suspense } from "hono/jsx/streaming"
+import { logger } from 'hono/logger'
 
 const app = new Hono()
 
-app.get('/page/*',
-    jsxRenderer(
-        ({ children }) => {
-            return (
-                <html>
-                    <body>
-                        <header>Menu</header>
-                        <div>{children}</div>
-                    </body>
-                </html>
-            )
-        },
-        {
-            docType: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
-        }
-    )
+const customLogger = (message: string, ...rest: string[]) => {
+    console.log(message, ...rest)
+}
+app.use(logger(customLogger))
+
+app.get(
+    '/page/*',
+    jsxRenderer(({ children }) => {
+        return (
+            <html>
+                <body>
+                    <header>Menu</header>
+                    <div>{children}</div>
+                </body>
+            </html>
+        )
+    },
+    {
+        docType: '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">'
+    })
 )
 
 app.get('/page/about', (c) => {
@@ -41,7 +47,6 @@ const AsyncComponent = async () => {
   await new Promise((r) => setTimeout(r, 1000))  // sleep 1second
   return <div>Hi!</div>
 }
-
 app.get(
   '/stream/*',
   jsxRenderer(
@@ -58,7 +63,6 @@ app.get(
     { stream: true}
   )
 )
-
 app.get('/stream', (c) => {
   return c.render(
     <Suspense fallback={<div>loading...</div>}>
@@ -66,5 +70,33 @@ app.get('/stream', (c) => {
     </Suspense>
   )
 })
+
+
+// Nested Layouts
+// The Layout component enables nesting the layouts
+app.use(
+    jsxRenderer(
+        ({ children }) => {
+            return (
+                <html>
+                    <body>{ children }</body>
+                </html>
+            )
+        }
+    )
+)
+const blog = new Hono()
+blog.use(
+    jsxRenderer(({ children, Layout }) => {
+        return (
+            <Layout>
+                <nav>Blog Menu</nav>
+                <div>{ children }</div>
+            </Layout>
+        )
+    })
+)
+app.route('/blog', blog)
+
 
 export default app
